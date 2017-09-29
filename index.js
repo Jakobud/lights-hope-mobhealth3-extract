85dff735c78f41009c0dc60e88a9bdeb3a00aee8
Jake Wilson <jake.e.wilson@gmail.com>
2017-09-29 12:56:12 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const fs = require('fs')
+
+// Database config
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'elysium'
+  },
+  pool: { min: 0, max: 7 }
+})
+
+let database = []
+let names = []
+let i
+let health
+
+knex
+  .select('c.entry', 'c.name', 'c.minlevel', 'c.maxlevel', 'c.minhealth', 'c.maxhealth')
+  .from('creature_template AS c')
+  .then(mobs => {
+
+    for (let mob of mobs) {
+      // Find average mob hitpoints
+      if (mob.minhealth !== mob.maxhealth) {
+        health = Math.round((mob.maxhealth + mob.minhealth) / 2)
+      } else {
+        health = Math.round(mob.minhealth)
+      }
+
+      // Push to database
+      for (i = mob.minlevel; i <= mob.maxlevel; i++) {
+        database[mob.name + ":" + i] = health
+        names.push(mob.name + ":" + i)
+      }
+    }
+
+    // Sort by name
+    names = names.sort()
+
+    // Write to lua file
+    let output = "MobHealth3DB = {\n"
+    for (let name of names) {
+      output += '\t["' + name.replace(/\"/g, '\\"') + '"] = ' + database[name] + ",\n"
+    }
+    output += "}\n"
+    fs.writeFile('MobHealth.lua', output)
+
+  })
+knex.destroy()
